a51c89f30a9a6b97a41920c112a2aac7c5b5e7a3
GustavoAFur <gustavoaragaof@gmail.com>
2025-03-09 18:28:53 -0300

    change initialize file

--- router/router.go
+++ router/router.go
@@ -1,14 +1,13 @@
 package router
 
 import (
-	"github.com/GustavoAFur/gogogo/router"
 	"github.com/gin-gonic/gin"
 )
 
 func Initialize() {
 	route := gin.Default()
 
-	router.Initialize(route)
+	initializeRoutes(route)
 
 	route.Run()
 }
